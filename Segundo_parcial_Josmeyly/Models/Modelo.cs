5b6bc5a0be28b51bd2769c6c387189933a465d19
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2025-03-09 22:07:15 -0400

    se creo el modelo

---
+++ Segundo_parcial_Josmeyly/Models/Modelo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Segundo_parcial_Josmeyly.Models
+{
+    public class Modelo
+    {
+
+        [Key]
+        public int ModeloId { get; set; }
+
+    }
+}
