dc6ca6baeb08c25b7fa3dadce14a7f7274d18d2a
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2025-03-09 22:07:39 -0400

    se creo lo services

---
+++ Segundo_parcial_Josmeyly/Services/ModeloServices.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq.Expressions;
+using Segundo_parcial_Josmeyly.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Segundo_parcial_Josmeyly.DAL;
+
+namespace Segundo_parcial_Josmeyly.Services
+{
+   
+        public class ModeloServices(IDbContextFactory<Contexto> DbFactory)
+        {
+            private async Task<bool> Existe(int Id)
+            {
+            return true;
+            }
+
+            private async Task<bool> Insertar(Modelo modelo)
+            {
+            return true;
+            }
+
+            private async Task<bool> Modificar(Modelo modelo)
+            {
+            return true;
+            }
+
+            public async Task<bool> Guardar(Modelo modelo)
+            {
+            return true;
+            }
+
+           public async Task<Modelo> Buscar(int Id)
+           {
+            
+           }
+
+            public async Task<bool> Eliminar(int modeloId)
+            {
+            return true;
+            }
+
+            public async Task<List<Modelo>> GetList(Expression<Func<Modelo, bool>> criterio)
+            {
+            
+            }
+           public async Task<List<Modelo>> GetModelo(int modeloId)
+           {
+            
+           }
+            public async Task<Modelo?> BuscarModelo(int modeloid)
+            {
+            
+            }
+        }
+
+
+    
+}
