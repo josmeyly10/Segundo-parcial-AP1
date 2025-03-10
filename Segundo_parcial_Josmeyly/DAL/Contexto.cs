bc5fd3802afb3de1bc6c632b649e33b1552b890d
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2025-03-09 22:08:42 -0400

    pages

---
+++ Segundo_parcial_Josmeyly/DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Segundo_parcial_Josmeyly.Models;
+using System.Collections.Generic;
+
+namespace Segundo_parcial_Josmeyly.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public virtual DbSet<Modelo> Modelo { get; set; }
+
+    }
+}
