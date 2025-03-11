8244520e692e7e332f80a3e88caa8d9f064f7d69
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2025-03-11 00:53:13 -0400

    se creo l modelo encuesta

--- Segundo_parcial_Josmeyly/Models/Encuesta.cs
+++ Segundo_parcial_Josmeyly/Models/Encuesta.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Segundo_parcial_Josmeyly.Models
 {
@@ -8,7 +9,20 @@ namespace Segundo_parcial_Josmeyly.Models
         [Key]
         public int EncuestaId { get; set; }
 
-       
 
+            public DateTime? Fecha { get; set; } = DateTime.Now;
+
+            [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "solo se permiten letras")]
+            public string? Asignatura { get; set; }
+            
+           public double Monto { get; set; }
+
+             
+            [InverseProperty("Encuesta")]
+            [ForeignKey("CiudadesId")]
+            public virtual ICollection<DetalleCiudad> DetalleCiudad { get; set; } = new List<DetalleCiudad>();
+
+            public virtual Ciudades Ciudades { get; set; } = null!;
+            public int CiudadesId { get; internal set; }
     }
 }
