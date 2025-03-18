5f7589bf44919218dc321d903bf5ae9f836c71ba
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2025-03-18 00:43:17 -0400

    se creo el modelo encuesta y se cambiaron algunas cosas

--- Segundo_parcial_Josmeyly/Models/Encuesta.cs
+++ Segundo_parcial_Josmeyly/Models/Encuesta.cs
@@ -5,24 +5,22 @@ namespace Segundo_parcial_Josmeyly.Models
 {
     public class Encuesta
     {
-
         [Key]
         public int EncuestaId { get; set; }
 
 
-            public DateTime? Fecha { get; set; } = DateTime.Now;
+        public DateTime? Fecha { get; set; } = DateTime.Now;
 
-            [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "solo se permiten letras")]
-            public string? Asignatura { get; set; }
-            
-           public double Monto { get; set; }
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "solo se permiten letras")]
+        public string? Asignatura { get; set; }
 
-             
-            [InverseProperty("Encuesta")]
-            [ForeignKey("CiudadesId")]
-            public virtual ICollection<DetalleCiudad> DetalleCiudad { get; set; } = new List<DetalleCiudad>();
+        public double Monto { get; set; }
 
-            public virtual Ciudades Ciudades { get; set; } = null!;
-            public int CiudadesId { get; internal set; }
+
+        [ForeignKey("CiudadesId")]
+        public virtual ICollection<DetalleCiudad> DetalleCiudad { get; set; } = new List<DetalleCiudad>();
+
+        public virtual Ciudades Ciudades { get; set; } = null!;
+        public int CiudadesId { get; internal set; }
     }
 }
