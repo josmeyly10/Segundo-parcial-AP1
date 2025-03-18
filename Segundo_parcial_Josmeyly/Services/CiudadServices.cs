26dd6268b25006ac7931a5800018bbced41dbf34
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2025-03-18 00:44:32 -0400

    se creo el servicesCiudad

---
+++ Segundo_parcial_Josmeyly/Services/CiudadServices.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+using Segundo_parcial_Josmeyly.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Segundo_parcial_Josmeyly.DAL;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Segundo_parcial_Josmeyly.Services
+{
+
+    public class CiudadServices(IDbContextFactory<Contexto> DbFactory)
+    {
+
+        public async Task<Ciudades> Buscar(int ciudadId)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Ciudades
+               // .Include(d => d.DetalleCiudad)
+                .FirstOrDefaultAsync(e => e.CiudadesId == ciudadId);
+        }
+
+        public async Task<List<Ciudades>> Listar(Expression<Func<Ciudades, bool>> criterio)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Ciudades
+                .Where(criterio)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
+
+
